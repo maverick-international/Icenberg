0b9e0b0cd3f42f46af7b24d80ff8be7939399bda
Dan Devine <dandevine0@gmail.com>
2022-07-02 15:40:52 +0100

    adds Url field

---
+++ src/Fields/Url.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MVRK\Icenberg\Fields;
+
+class Url extends Base
+{
+    /**
+     * Scaffold a text field
+     *
+     * @param object $field
+     * @param string $layout the current row layout
+     * @return string
+     */
+    public function getElement($field, $layout, $tag)
+    {
+        $name = $field['_name'];
+
+        $content = get_sub_field($name);
+
+        $wrapped = $this->wrap($content, $name, $layout, $tag);
+
+        return $wrapped;
+    }
+}
