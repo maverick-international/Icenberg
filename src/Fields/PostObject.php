c2dc2b8b296c5a9547194d3117314acd5911aa33
Dan Devine <dandevine0@gmail.com>
2022-09-07 17:47:03 +0100

    adds po

--- src/Fields/PostObject.php
+++ src/Fields/PostObject.php
@@ -4,7 +4,63 @@ namespace MVRK\Icenberg\Fields;
 
 class PostObject extends Base
 {
-    public function getElement()
+
+
+    public function getElement($field, $layout, $tag)
     {
+        $name = $field['_name'];
+
+        $post_object_field = get_sub_field($name);
+
+        if (!$post_object_field) {
+            return;
+        }
+
+        // if the field is set to only return an ID then just return the ID
+        if (is_int($post_object_field)) {
+            return $post_object_field;
+        }
+
+        // if multiple posts have been enabled
+        if (is_array($post_object_field)) {
+            $content = '';
+            foreach ($post_object_field as $post_object) {
+                $content .=  $this->buildPostContent($post_object, $name, $layout, $tag);
+            }
+        }
+
+
+        // field using default behaviour and returning a single post object
+        if (is_object($post_object_field)) {
+            $content =  $this->buildPostContent($post_object_field, $name, $layout, $tag);
+        }
+
+        dump($content);
+
+        return $this->wrap($content, $name, $layout, $tag);
+    }
+
+
+    protected function buildPostContent($post_object, $name, $layout, $tag)
+    {
+        $content = [];
+
+        $content['title'] = $post_object->post_title;
+
+        $content['modified'] = $post_object->post_modified;
+
+        $content['excerpt'] = $post_object->post_excerpt;
+
+        $date = new \DateTime($content['modified']);
+
+        $post_content = $this->wrap($date->format('jS F Y'), $name . '__date', $layout, $tag);
+
+        $post_content .=  $this->wrap($content['title'], $name . '__title', $layout, 'h4');
+
+        $post_content .=  $this->wrap($content['excerpt'], $name . '__excerpt', $layout, 'p');
+
+        $post_wrap = $this->wrap($post_content, $name . '__item', $layout, $tag);
+
+        return $post_wrap;
     }
 }
