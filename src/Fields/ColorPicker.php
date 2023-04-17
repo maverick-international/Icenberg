a0baa3c06c34a6093b852be59fcd8eb07ae8106e
“Tasha <natasharegan2@gmail.com>
2023-04-17 10:26:11 +0100

    add colour picker to to icenberg

---
+++ src/Fields/ColorPicker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MVRK\Icenberg\Fields;
+
+class ColorPicker extends Base
+{
+    public function getElement($field, $layout, $tag)
+    {
+        $name = $field['_name'];
+
+        $content = get_sub_field($name);
+
+        if (is_iterable($content)) {
+            $wrapped = $this->listWrap($content, $name, $layout);
+        } else {
+            $wrapped = $this->wrap($content, $name, $layout, $tag);
+        }
+
+        return $wrapped;
+    }
+}
