d302aee7ce74de07d061e9b6367eac6269d2d8b6
Dan Devine <dan.devine@maverick-intl.com>
2023-05-23 11:53:45 +0100

    adds page link field

--- src/Fields/PageLink.php
+++ src/Fields/PageLink.php
@@ -4,4 +4,14 @@ namespace MVRK\Icenberg\Fields;
 
 class PageLink extends Base
 {
+    public function getElement($field, $layout, $tag)
+    {
+        $name = $field['_name'];
+
+        $link = get_sub_field($name);
+
+        $wrapped = "<a class='block--{$this->unSnake($layout)}__{$this->unSnake($name)}' href='{$link}'>{$link}</a>";
+
+        return $wrapped;
+    }
 }
