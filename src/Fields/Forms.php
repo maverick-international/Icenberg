97134689da6161f1587160d3a7af8a938da3c55d
Dan Devine <dandevine0@gmail.com>
2025-05-12 09:37:27 +0100

    simplifies form field

--- src/Fields/Forms.php
+++ src/Fields/Forms.php
@@ -4,64 +4,24 @@ namespace MVRK\Icenberg\Fields;
 
 class Forms extends Base
 {
-
-    protected $titles;
+    public $field_object;
 
     public $icenberg;
 
-    protected $title;
+    public $tag;
 
-    protected $description;
+    public $post_id;
 
-    protected $form;
-
-    public $gravity_form;
-
-    protected $tag;
-
-    protected $name;
-
-    protected $modifiers;
+    public $modifiers;
 
     public function getElement($field_object, $icenberg, $tag, $post_id, $modifiers = [])
     {
-        $this->form = $field_object['value'];
+        $form = $field_object['value'];
 
-        $this->name = $field_object['_name'];
+        $name = $field_object['_name'];
 
-        $this->icenberg = $icenberg;
+        $gravity_form = gravity_form($form['id'], $display_title = false, $display_description = false, $display_inactive = false, $field_values = null, $ajax = true, $tabindex = 0, $echo = false);
 
-        $this->tag = $tag;
-
-        $this->modifiers = $modifiers;
-
-        $this->getTitles();
-
-        $this->gravity_form = gravity_form($this->form['id'], $display_title = false, $display_description = false, $display_inactive = false, $field_values = null, $ajax = true, $tabindex = 0, $echo = false);
-
-        return $this->build();
-    }
-
-    protected function getTitles()
-    {
-        $this->title = $this->form['title'];
-
-        $this->description = $this->form['description'];
-
-        $content =  $this->wrap($this->title, $this->name . '__heading', $this->icenberg, 'h3');
-
-        $content .=  $this->wrap($this->description, $this->name . '__description', $this->icenberg, 'p');
-
-        $this->titles =  $this->wrap($content, $this->name . '__titles', $this->icenberg, $this->tag);
-    }
-
-    public function build()
-    {
-
-        $content = $this->titles;
-
-        $content .=  $this->wrap($this->gravity_form, $this->name . '__form', $this->icenberg, $this->tag);
-
-        return $this->wrap($content, $this->name, $this->icenberg, $this->tag, $this->modifiers);
+        return $this->wrap($gravity_form, $name, $icenberg, $tag, $modifiers);
     }
 }
