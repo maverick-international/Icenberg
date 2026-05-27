666b198cd81fd8d55c873624ba108a5b603fa8c5
Dan Devine <dandevine0@gmail.com>
2026-05-27 11:14:11 +0100

    stops nested elements being converted to modifiers by ignoring double underscores in kebebcase conversion

--- src/Utils/Format.php
+++ src/Utils/Format.php
@@ -6,9 +6,12 @@ class Format
 {
     public static function kebabCase(string $string): string
     {
+        // second regex preserves double underscores so that in nested contexts
+        // like groups and repeaters the elements aren't converted to modifiers!
         $result = preg_replace('/([a-z])([A-Z])/', '$1-$2', $string);
+        $result = preg_replace('/(?<!_)_(?!_)/', '-', $result);
 
-        return strtolower(str_replace('_', '-', $result));
+        return strtolower($result);
     }
 
     public static function pascalCase(string $string): string
